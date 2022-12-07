3edf6939fa340e9909b988626b4105f0fdb2ab5b
i338778 <i338778@lab107-20.stud.ii>
2022-12-07 09:10:34 +0100

    naprawienie programu i przywrocenie starych testow

--- tests.pl
+++ tests.pl
@@ -6,9 +6,14 @@ test(project_dedalus_quicksort,[nondet]) :-
   project_dedalus_quicksort(L, Sorted), 
   sort(L, Sorted).
 
-test(quicksort,[nondet]) :-
-  L=[8,4,2,1,3,6,5,7,12,10,9,11,14,13,15],
-  project_dedalus_quicksort(L, Sorted),
+% test(quicksort,[nondet]) :-
+%  L=[8,4,2,1,3,6,5,7,12,10,9,11,14,13,15],
+%  project_dedalus_quicksort(L, Sorted),
+%  sort(L, Sorted).
+
+test(project_dedalus_quicksort,[nondet]) :-
+  randseq(15,15,L),
+  project_dedalus_quicksort(L, Sorted), 
   sort(L, Sorted).
 
 :- end_tests(qs).
