2eb74af6c768916062456f03b3aea98b34f93415
i338439 <i338439@lab109-10.stud.ii>
2022-12-13 16:05:26 +0100

    coś2

--- tests.pl
+++ tests.pl
@@ -1,7 +1,7 @@
 :- begin_tests(qs).
 :- use_module(library(lists)).
 
-test(project_dedalus_quicksort,[nondet]) :-
+test(quicksort,[nondet]) :-
   L = [4,5,1],
   project_dedalus_quicksort(L, Sorted), 
   sort(L, Sorted).
