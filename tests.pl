c6a0486f855e40260b095914cb371003f92ed79f
i345906 <i345906@lab108-06.stud.ii>
2023-12-07 20:29:21 +0100

    poprawa błędnych plików testowych

--- tests.pl
+++ tests.pl
@@ -6,7 +6,7 @@ test(project_dedalus_quicksort,[nondet]) :-
   project_dedalus_quicksort(L, Sorted), 
   sort(L, Sorted).
 
-test(quicksort,[nondet]) :-
+test(project_dedalus_quicksort,[nondet]) :-
   L=[8,4,2,1,3,6,5,7,12,10,9,11,14,13,15],
   project_dedalus_quicksort(L, Sorted),
   sort(L, Sorted).
